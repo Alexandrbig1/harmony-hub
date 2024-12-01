d6f298d103c778472b00874888341413258cccd9
DripTrace <rpalm@driptrace.io>
2024-12-01 12:49:28 -0600

    adding test endpoint

--- src/pages/MusicHub/MusicHub.jsx
+++ src/pages/MusicHub/MusicHub.jsx
@@ -4,135 +4,145 @@ import questionsData from "@/data/questions.json";
 import { toast } from "react-toastify";
 import { commonToastOptions } from "../../helpers/toastOptions";
 import {
-  MusicHubWrapper,
-  QuestionWrapper,
-  StepCounter,
-  QuestionText,
-  AnswerInput,
-  NextButton,
-  MusicFormWrapper,
-  MusicTextWrapper,
-  MusicTitle,
-  RedirectButton,
-  ResetButton,
-  ButtonWrapper,
-  MusicHubEffectBg,
+	MusicHubWrapper,
+	QuestionWrapper,
+	StepCounter,
+	QuestionText,
+	AnswerInput,
+	NextButton,
+	MusicFormWrapper,
+	MusicTextWrapper,
+	MusicTitle,
+	RedirectButton,
+	ResetButton,
+	ButtonWrapper,
+	MusicHubEffectBg,
 } from "./MusicHub.styled";
 
 function Playlist() {
-  const { user } = useAuth();
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answers, setAnswers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [redirectUrl, setRedirectUrl] = useState("");
+	const { user } = useAuth();
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [answers, setAnswers] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [redirectUrl, setRedirectUrl] = useState("");
 
-  const handleAnswerChange = (e) => {
-    const newAnswers = [...answers];
-    newAnswers[currentQuestion] = e.target.value;
-    setAnswers(newAnswers);
-  };
+	const handleAnswerChange = (e) => {
+		const newAnswers = [...answers];
+		newAnswers[currentQuestion] = e.target.value;
+		setAnswers(newAnswers);
+	};
 
-  const handleNextQuestion = () => {
-    if (loading) return;
+	const handleNextQuestion = () => {
+		if (loading) return;
 
-    if (currentQuestion < questionsData.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-      return;
-    }
+		if (currentQuestion < questionsData.length - 1) {
+			setCurrentQuestion(currentQuestion + 1);
+			return;
+		}
 
-    setLoading(true);
-    toast.info("Processing your ringtone, please wait...", commonToastOptions);
+		setLoading(true);
+		toast.info(
+			"Processing your ringtone, please wait...",
+			commonToastOptions
+		);
 
-    fetch(
-      `https://737f-50-171-3-130.ngrok-free.app/api/generate?prompt=${encodeURIComponent(
-        answers.join(". ")
-      )}`,
-      {
-        method: "GET",
-        redirect: "follow",
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-        },
-      }
-    )
-      .then((response) => {
-        const contentType = response.headers.get("content-type");
-        if (
-          response.ok &&
-          contentType &&
-          contentType.includes("application/json")
-        ) {
-          return response.json();
-        } else {
-          return response.text().then((errorText) => {
-            throw new Error("Failed to generate the playlist.");
-          });
-        }
-      })
-      .then((data) => {
-        toast.success(
-          "Your personalized playlist is ready!",
-          commonToastOptions
-        );
-        setRedirectUrl(data.url);
-      })
-      .catch((error) => {
-        toast.error(
-          "An error occurred. Please try again later.",
-          commonToastOptions
-        );
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+		fetch(
+			`${
+				import.meta.env.APP_API_URL
+			}/api/generate?prompt=${encodeURIComponent(answers.join(". "))}`,
+			{
+				method: "GET",
+				redirect: "follow",
+				headers: {
+					"ngrok-skip-browser-warning": "69420",
+				},
+			}
+		)
+			.then((response) => {
+				const contentType = response.headers.get("content-type");
+				if (
+					response.ok &&
+					contentType &&
+					contentType.includes("application/json")
+				) {
+					return response.json();
+				} else {
+					return response.text().then((errorText) => {
+						throw new Error("Failed to generate the playlist.");
+					});
+				}
+			})
+			.then((data) => {
+				toast.success(
+					"Your personalized playlist is ready!",
+					commonToastOptions
+				);
+				setRedirectUrl(data.url);
+			})
+			.catch((error) => {
+				toast.error(
+					"An error occurred. Please try again later.",
+					commonToastOptions
+				);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	};
 
-  const handleReset = () => {
-    setCurrentQuestion(0);
-    setAnswers([]);
-    setRedirectUrl("");
-  };
+	const handleReset = () => {
+		setCurrentQuestion(0);
+		setAnswers([]);
+		setRedirectUrl("");
+	};
 
-  const { question, example } = questionsData[currentQuestion];
+	const { question, example } = questionsData[currentQuestion];
 
-  return (
-    <MusicHubWrapper>
-      <MusicTextWrapper>
-        <MusicHubEffectBg />
-        <MusicTitle>Hello, {user.name}!</MusicTitle>
+	return (
+		<MusicHubWrapper>
+			<MusicTextWrapper>
+				<MusicHubEffectBg />
+				<MusicTitle>Hello, {user.name}!</MusicTitle>
 
-        <MusicFormWrapper>
-          <StepCounter>
-            {currentQuestion + 1} of {questionsData.length}
-          </StepCounter>
-          <QuestionWrapper>
-            <QuestionText>{question}</QuestionText>
-            <AnswerInput
-              type="text"
-              value={answers[currentQuestion] || ""}
-              onChange={handleAnswerChange}
-              placeholder={example ? example : "Type your answer here"}
-            />
-          </QuestionWrapper>
-          <ButtonWrapper>
-            <NextButton onClick={handleNextQuestion} disabled={loading}>
-              {loading
-                ? "Generating..."
-                : currentQuestion === questionsData.length - 1
-                ? "Generate"
-                : "Next"}
-            </NextButton>
-            <ResetButton onClick={handleReset}>Reset</ResetButton>
-          </ButtonWrapper>
-          {redirectUrl && (
-            <RedirectButton onClick={() => window.open(redirectUrl, "_blank")}>
-              Go to Your Ringtone
-            </RedirectButton>
-          )}
-        </MusicFormWrapper>
-      </MusicTextWrapper>
-    </MusicHubWrapper>
-  );
+				<MusicFormWrapper>
+					<StepCounter>
+						{currentQuestion + 1} of {questionsData.length}
+					</StepCounter>
+					<QuestionWrapper>
+						<QuestionText>{question}</QuestionText>
+						<AnswerInput
+							type="text"
+							value={answers[currentQuestion] || ""}
+							onChange={handleAnswerChange}
+							placeholder={
+								example ? example : "Type your answer here"
+							}
+						/>
+					</QuestionWrapper>
+					<ButtonWrapper>
+						<NextButton
+							onClick={handleNextQuestion}
+							disabled={loading}
+						>
+							{loading
+								? "Generating..."
+								: currentQuestion === questionsData.length - 1
+								? "Generate"
+								: "Next"}
+						</NextButton>
+						<ResetButton onClick={handleReset}>Reset</ResetButton>
+					</ButtonWrapper>
+					{redirectUrl && (
+						<RedirectButton
+							onClick={() => window.open(redirectUrl, "_blank")}
+						>
+							Go to Your Ringtone
+						</RedirectButton>
+					)}
+				</MusicFormWrapper>
+			</MusicTextWrapper>
+		</MusicHubWrapper>
+	);
 }
 
 export default Playlist;
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,2 +1,5 @@
 // export const URL = "https://harmony-hub-backend.onrender.com/api";
-export const URL = `${process.env.APP_API_URI}/api`;
+// export const URL = `${process.env.APP_API_URI}/api`;
+// export const URL = "http://localhost:27017/api"
+// export const URL = `${import.meta.env.APP_API_URL}/api`
+export const URL = "https://harmonyhub-vzfi.onrender.com/api"
